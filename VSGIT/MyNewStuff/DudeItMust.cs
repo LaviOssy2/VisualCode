f741ca0256cf83d9b95065a7a39b39861ac9ce2d
LaviOssy2 <ossy@post.bgu.ac.il>
2023-04-19 09:46:16 +0300

    that is super werd

---
+++ VSGIT/MyNewStuff/DudeItMust.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSGIT.MyNewStuff
+{
+    internal class DudeItMust
+    {
+        public static void Main()
+        {
+            Console.WriteLine("the fuck??");
+        }
+    }
+}
