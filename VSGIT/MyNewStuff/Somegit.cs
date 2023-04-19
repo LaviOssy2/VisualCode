8a36405ddcd3daab1c0d13faf171e3c044da159f
LaviOssy2 <ossy@post.bgu.ac.il>
2023-04-19 09:39:58 +0300

    stange but okay

---
+++ VSGIT/MyNewStuff/Somegit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VSGIT.MyNewStuff
+{
+    public class Somegit
+    {
+        public static void Main()
+        {
+            int a = 0;
+            Console.Write(a);
+        }
+    }
+}
